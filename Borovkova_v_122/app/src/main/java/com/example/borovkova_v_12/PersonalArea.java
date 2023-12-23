5ea80e754c484e5c7176cfb3905632a302808785
Sofa2003 <sborovkova168@gmail.com>
2023-12-23 14:09:25 +0700

    End commit

--- Borovkova_v_122/app/src/main/java/com/example/borovkova_v_12/PersonalArea.java
+++ Borovkova_v_122/app/src/main/java/com/example/borovkova_v_12/PersonalArea.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,18 +19,21 @@ public class PersonalArea extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.personal_area);
         ImageView imageView = (ImageView) findViewById(R.id.imageView5);
         imageView.setOnClickListener(this);
-        ImageView imageView1 =(ImageView) findViewById(R.id.imageView6);
-        imageView1.setOnClickListener(this);
+        TextView textView =(TextView) findViewById(R.id.textView5);
+        textView.setOnClickListener(this);
     }
     public void onClickAdress(View view) {
         startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("geo:")));
     }
 
     @Override
-    public void onClick(View v){
-        Intent intent = new Intent(view.getContext(),Login.class);
-        startActivity(intent);
-        Intent intent1 = new Intent(view.getContext(), Search.class);
-        startActivity(intent1);
+    public void onClick(View v) {
+        if (v.getId() == R.id.imageView5) {
+            Intent intent = new Intent(this, Login.class);
+            startActivity(intent);
+        } else if (v.getId() == R.id.textView5) {
+            Intent intent1 = new Intent(this, Search.class);
+            startActivity(intent1);
+        }
     }
 }
--- Borovkova_v_122/app/src/main/java/com/example/borovkova_v_12/Search.java
+++ Borovkova_v_122/app/src/main/java/com/example/borovkova_v_12/Search.java
@@ -1,13 +1,31 @@
 package com.example.borovkova_v_12;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Search extends AppCompatActivity {
+public class Search extends AppCompatActivity implements View.OnClickListener{
+    private View view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search);
+        ImageView imageView=(ImageView) findViewById(R.id.imageView8);
+        imageView.setOnClickListener(this);
+        ImageView imageView1=(ImageView) findViewById(R.id.imageView9);
+        imageView1.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.imageView8) {
+            Intent intent = new Intent(this, PersonalArea.class);
+            startActivity(intent);
+        } else if (v.getId() == R.id.imageView9) {
+            Intent intent1 = new Intent(this, Login.class);
+            startActivity(intent1);
+        }
     }
 }
